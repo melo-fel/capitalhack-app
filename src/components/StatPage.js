89e4720636827f1d7c9bd2348dc0abf1077a90d2
Felipe <articmario101@gmail.com>
2023-05-19 02:55:01 -0400

    StatPage updated for graph

--- src/components/StatPage.js
+++ src/components/StatPage.js
@@ -2,6 +2,36 @@ const StatPage = () => {
     return (
         <div>
             <h1 className="title">Statistics</h1>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+
+            <body>
+                <canvas id="myChart" style="width:100%;max-width:600px"></canvas>
+                <script>
+                    var xValues = ["Sarah", "Messenger", "Snake vs Block", "Facebook", "Amazon", "Spotify Music", "Netflix", "Word Connect", "Pandora", "WhatsApp Messenger"];
+                    var yValues = [388, 257, 242, 214, 183, 154, 153, 144, 130, 121];
+                    var barColors = ["red", "green","blue","orange","brown"];
+                    
+                    new Chart("myChart", {
+                    type: "horizontalBar",
+                    data: {
+                        labels: xValues,
+                        datasets: [{
+                            backgroundColor: barColors,
+                            data: yValues
+                        }]
+                    },
+                    options: {
+                        legend: {display: false},
+                        title: {
+                            display: true,
+                            text: "Graph of Monetary Statistics"
+                        },
+                        scales: {
+                            xAxes: [{ticks: {min: 100, max:400}}]
+                        }
+                        }});
+                </script>
+            </body>
         </div>
     );
 }
