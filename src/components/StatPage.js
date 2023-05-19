bcaeb6127d83661d5216d6ded9ccf0606e2ad245
Felipe <articmario101@gmail.com>
2023-05-19 10:57:37 -0400

    Updated Statspage file

--- src/components/StatPage.js
+++ src/components/StatPage.js
@@ -2,36 +2,19 @@ const StatPage = () => {
     return (
         <div>
             <h1 className="title">Statistics</h1>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+            <canvas id = "statsGraph" style="width:100%;max-width:700px"></canvas>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">
 
-            <body>
-                <canvas id="myChart" style="width:100%;max-width:600px"></canvas>
-                <script>
-                    var xValues = ["Sarah", "Messenger", "Snake vs Block", "Facebook", "Amazon", "Spotify Music", "Netflix", "Word Connect", "Pandora", "WhatsApp Messenger"];
-                    var yValues = [388, 257, 242, 214, 183, 154, 153, 144, 130, 121];
-                    var barColors = ["red", "green","blue","orange","brown"];
-                    
-                    new Chart("myChart", {
-                    type: "horizontalBar",
-                    data: {
-                        labels: xValues,
-                        datasets: [{
-                            backgroundColor: barColors,
-                            data: yValues
-                        }]
-                    },
-                    options: {
-                        legend: {display: false},
-                        title: {
-                            display: true,
-                            text: "Graph of Monetary Statistics"
-                        },
-                        scales: {
-                            xAxes: [{ticks: {min: 100, max:400}}]
-                        }
-                        }});
-                </script>
-            </body>
+            var xValues = ["Sarah", "Messenger", "Snake vs Block", "Facebook", "Amazon", "Spotify Music", "Netflix", "Word Connect", "Pandora", "WhatsApp Messenger"];
+            var yValues = [55, 49, 44, 24, 15, 55, 49, 44, 24, 15];
+            var barColors = ["red", "green","blue","orange","brown"];
+
+            const myChart = new Chart("myChart", {
+                type: bar,
+                data: {labels: xValues, datasets: [{backgroundColor: barColors, data: yValues{"}"}]{"}"},
+                options: {legend: {display: false{"}"}, title: {display: true, text: "Stats Page for User"}}
+            {"}"});
+            </script>
         </div>
     );
 }
